21191958260bfa4235fe6f78baeb7b5c1cd67467
Felipe Alonso Rodriguez Aranda <farodrig@pudu>
2015-11-18 11:18:20 -0300

    arreglado client.sh para ser ejecutado

--- JuegoSSDD-master/client.sh
+++ JuegoSSDD-master/client.sh
@@ -1,14 +1,15 @@
+
 if [ -z "$1" ]
   then echo "Debe ingresar la IP del servidor donde conectarse."
   exit
 fi
 # Compilación de archivos java
 echo "Compilando programa."
-javac src/cl/uchile/dcc/cc5303/*.java src/cl/uchile/dcc/cc5303/*.java
+javac -cp ".:jar/sigar.jar" src/cl/uchile/dcc/cc5303/*.java
 # Creación de directorio para archivos binarios de java
 mkdir -p bin/cl/uchile/dcc/cc5303/
 mv src/cl/uchile/dcc/cc5303/*.class bin/cl/uchile/dcc/cc5303/
 # Correr el cliente
 echo "Iniciando Juego"
 cd bin
-java -Djava.rmi.server.hostname=$1 cl.uchile.dcc.cc5303.Client $1
+java -cp ".:../jar/sigar.jar" -Djava.rmi.server.hostname=$1 cl.uchile.dcc.cc5303.Client $1
